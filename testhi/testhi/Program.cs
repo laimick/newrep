468a51249a86a1b8751beb9c9eb382f7ba446b12
laimick <kirill.perih.2003@gmail.com>
2022-02-07 14:40:14 +0500

    justhello

---
+++ testhi/testhi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testhi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
